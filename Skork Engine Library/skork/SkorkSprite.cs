9706d3dfd3f842cb65d551ccbc571c4ca8b949ed
Reapism <reapsprgm@gmail.com>
2019-08-07 23:00:01 -0400

    An implementation of the new skork interfaces

--- Skork Engine Library/skork/SkorkSprite.cs
+++ Skork Engine Library/skork/SkorkSprite.cs
@@ -1,17 +1,41 @@
-﻿using SkorkEngine.exception;
+﻿using Skork_Engine_Library.Skork;
+using SkorkEngine.exception;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace Skork_Engine_Library.skork {
-    public class SkorkSprite {
+    /// <summary>
+    /// The <see cref="SkorkSprite"/> class.
+    /// <para>Represents a single instance of a Sprite.</para>
+    /// Implements <seealso cref="ISkorkEntity"/>,
+    /// <seealso cref="ISkorkMovable{Entity}"/>.
+    /// </summary>
+    public class SkorkSprite : ISkorkEntity, ISkorkMovable<ISkorkEntity> {
 
-        private string name;
-        private Image spriteImg;
-        private Point position;
+        #region Instance member(s)
+
+        #endregion
+
+        #region Static Fields
 
         private static int ACTIVE_SPRITE_INSTANCES;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The name of the sprite.
+        /// </summary>
+        public string z { get; set; }
+        public Point Position { get; set; }
+        public Image SpriteImage { get; set; }
+
+        #endregion
+
+        #region Contructors & Destructors
 
         /// <summary>
         /// Initialize instances to 0.
@@ -29,16 +53,22 @@ namespace Skork_Engine_Library.skork {
         }
 
         /// <summary>
-        /// Default 
+        /// Default constructor.
         /// </summary>
         /// <param name="name"></param>
         public SkorkSprite(string name) : this(name, new Point(0, 0)) {
 
         }
 
+        /// <summary>
+        /// Initializes the a <see cref="SkorkSprite"/> instance with a
+        /// <paramref name="name"/> and <paramref name="position"/>.
+        /// </summary>
+        /// <param name="name">The name of the sprite.</param>
+        /// <param name="position">The position of the sprite.</param>
         public SkorkSprite(string name, Point position) {
-            this.name = name;
-            this.position = position;
+            this.Name = name;
+            this.Position = position;
             this.spriteImg = new Image();
 
             if (ACTIVE_SPRITE_INSTANCES != Properties.Settings.Default.max_sprite_instances) {
@@ -48,6 +78,9 @@ namespace Skork_Engine_Library.skork {
             }
         }
 
+        #endregion
+
+        #region Static methods and properties
 
         /// <summary>
         /// Returns the current number of active sprite instances.
@@ -55,5 +88,34 @@ namespace Skork_Engine_Library.skork {
         public int GetActiveSpriteInstances {
             get => ACTIVE_SPRITE_INSTANCES;
         }
+
+        #endregion
+
+        #region Instance methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="units"></param>
+        public void Up(ISkorkEntity entity, int units) {
+            if (entity is SkorkSprite) {
+
+            }
+        }
+
+        public void Down(ISkorkEntity entity, int units) {
+            throw new System.NotImplementedException();
+        }
+
+        public void Left(ISkorkEntity entity, int units) {
+            throw new System.NotImplementedException();
+        }
+
+        public void Right(ISkorkEntity entity, int units) {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
     }
 }
