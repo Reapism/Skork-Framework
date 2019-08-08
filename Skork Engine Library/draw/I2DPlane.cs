c91954f963f4dc8d2b4be68abbee9de414953901
Reapism <reapsprgm@gmail.com>
2019-08-08 00:01:14 -0400

    Interface for any 2D plane.

---
+++ Skork Engine Library/draw/I2DPlane.cs
@@ -0,0 +1,15 @@
+﻿namespace Skork_Engine_Library.Draw {
+
+    /// <summary>
+    /// The <see cref="I2DPlane"/> interface.
+    /// <para>An interface representing a 2D plane
+    /// using Height and Width properties.</para>
+    /// </summary>
+    public interface I2DPlane {
+
+        int Height { get; set; }
+
+        int Width { get; set; }
+
+    }
+}
