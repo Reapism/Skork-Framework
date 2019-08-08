66c48526edbbf2afdbbcbb49d777d3b0a271d113
Reapism <reapsprgm@gmail.com>
2019-08-07 22:59:42 -0400

    Added ISkorkMovable, ISkorkEntity interfaces for SOLID design in programming. And code reusability.

---
+++ Skork Engine Library/skork/ISkorkEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Skork_Engine_Library.Skork {
+
+    /// <summary>
+    /// Fundamental properties for any <see cref="ISkorkEntity"/>.
+    /// </summary>
+    public interface ISkorkEntity {
+
+        /// <summary>
+        /// The name associated with this entity.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// The 2D position of the entity.
+        /// </summary>
+        Point Position { get; set; }
+    }
+}
