d1ec925562b5552154325290b8cecb6a5391ff65
Anthony J <reapsprgm@gmail.com>
2019-10-03 23:04:21 -0400

    Adding interfaces used for the models.

---
+++ Skork.Data/Interfaces/IModification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skork.Data.Interfaces
+{
+    public interface IModification
+    {
+        /// <summary>
+        /// The <see cref="DateTime"/> that was last
+        /// modified by.
+        /// </summary>
+        DateTime DateModified { get; set; }
+
+        /// <summary>
+        /// The <see cref="DateTime"/> that was 
+        /// created by.
+        /// </summary>
+        DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// Represents if a model has been modified.
+        /// </summary>
+        bool IsModified { get; set; }
+
+    }
+}
