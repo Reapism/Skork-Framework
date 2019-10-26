1aaf0fea026dce5e1a576be1466331312e603793
Anthony J <reapsprgm@gmail.com>
2019-10-26 19:33:21 -0400

    refactoring SkorkParser to SRP.

---
+++ API/Skork.Language.Parse/Interfaces/ISkorkParser.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Skork.Language.Parse.Interfaces
+{
+    public interface ISkorkParser
+    {
+        List<string> PotentialCode { get; set; }
+
+        List<string> Parse();
+    }
+}
