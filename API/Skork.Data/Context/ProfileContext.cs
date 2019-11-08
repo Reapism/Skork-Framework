20f845d032c6b8ad5985e9e6d11672a325765d3f
Anthony J <reapsprgm@gmail.com>
2019-11-07 20:23:23 -0500

    added more contexts

--- API/Skork.Data/Context/ProfileContext.cs
+++ API/Skork.Data/Context/ProfileContext.cs
@@ -6,5 +6,15 @@ namespace Skork.Data.Context
     public class ProfileContext : DbContext
     {
         public DbSet<IProfile> Profiles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- API/Skork.Data/Context/ProjectContext.cs
+++ API/Skork.Data/Context/ProjectContext.cs
@@ -6,5 +6,11 @@ namespace Skork.Data.Context
     public class ProjectContext : DbContext
     {
         public DbSet<IProject> Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
     }
 }
